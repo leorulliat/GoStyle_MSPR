e9ef87ddfbdffaed2401ca19f663ddae993c39ef
leorulliat <57667446+leorulliat@users.noreply.github.com>
2020-04-14 11:47:11 +0200

    Add files via upload
    premier test api

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-//v1 api
+var express = require('express');
+var app = express();
+
+// '/' est la route racine
+app.get('/', function (req, res) {
+    res.send("hello world!");
+});
+
+app.listen(4000, function () {
+    console.log("En écoute sur le port 4000");
+});
