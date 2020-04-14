4723b0f9c029e24d1c54f5ae2d033dd06f83d1b7
leorulliat <57667446+leorulliat@users.noreply.github.com>
2020-04-14 12:00:46 +0200

    Add files via upload
    récupération de données depuis la BDD

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 // '/' est la route racine
 app.get('/', function (req, res) {
-    res.send("hello world!");
+    // connexion à la base de donnée
+    var mySqlClient = mysql.createConnection({
+        host     : "localhost",
+        user     : "root",
+        password : "",
+        database : "go_style_project_db"
+    });
+
+    mySqlClient.query(
+        "SELECT code FROM codes_promo WHERE uuid = '98670694493061125'",       // réponse attendue CODETEST1
+        function select(error, results) {
+            if (error) {
+                console.log('error SQL :',error);
+                mySqlClient.end();
+                return;
+            }
+            
+            if ( results.length > 0 )  { 
+                var firstResult = results[0];
+                res.send(firstResult);
+                console.log('reponse : ',firstResult);
+                
+            } else {
+                res.send('Pas de données');
+                console.log('reponse : ',"Pas de données");
+            }
+            mySqlClient.end();
+        }
+    );
 });
 
 app.listen(4000, function () {
